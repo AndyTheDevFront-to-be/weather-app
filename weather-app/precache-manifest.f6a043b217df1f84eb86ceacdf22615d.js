f3e3baacb2718526be993ba9f5df973bfe483b67
Andy <32070305+AndyTheDevFront-to-be@users.noreply.github.com>
2019-03-18 12:01:34 +0100

    Add files via upload

---
+++ weather-app/precache-manifest.f6a043b217df1f84eb86ceacdf22615d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "fefba7826eaf9d9509ea",
+    "url": "/static/css/main.5630245e.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "c2bd045b0a248efcca0b",
+    "url": "/static/js/2.00f7bf07.chunk.js"
+  },
+  {
+    "revision": "fefba7826eaf9d9509ea",
+    "url": "/static/js/main.c8b714fe.chunk.js"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "c2bd045b0a248efcca0b",
+    "url": "/static/css/2.01728680.chunk.css"
+  },
+  {
+    "revision": "e478538af560157bbab96ce9a2b06954",
+    "url": "/index.html"
+  }
+];
